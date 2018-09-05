f7f40a88cf702f4c8d09d0313a8293df89c743d9
nooha mohammed <noohamohammed00@gmail.com>
2018-09-05 18:27:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+
+function findMatching(drivers,name){
+  
+   return drivers.filter(function (n){
+    return  n.toUpperCase() === name.toUpperCase() 
+  })
+  
+}
+
+function fuzzyMatch(drivers,letter){
+  partOfName = letter.length
+  return drivers.filter(function (name){
+    return name.slice(0,partOfName) === letter
+  })
+}
+
+function matchName (list, name) {
+  return list.filter(function (driver) {
+    return driver.name.toLowerCase() === name.toLowerCase();
+  });
+}
